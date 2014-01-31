be21351b2612ca08eb817572ecfe4acfe1f4d3bb
Abhishek-kushwaha <akushwaha@q3tech.com>
2014-01-31 11:16:20 +0530

    removed show links from index and show view

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,7 +6,7 @@ class TopicsController < ApplicationController
   end
 
   def new
-    @topic = Topic.new
+    @topic = Topic.new(:name => params[:topic][:name], :last_poster_id => current_user.id, :last_post_at => Time.now, :forum_id => params[:topic][:forum_id], :user_id => current_user.id)
     @post = Post.new
   end
 
